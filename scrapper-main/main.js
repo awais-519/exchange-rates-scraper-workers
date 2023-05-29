e579cae8a294f8b42c51be472dd48b390be51353
awais-519 <awaisul519@gmail.com>
2023-05-30 01:11:21 +0500

    Formatted Data and Initialised DB Worker

--- scrapper-main/main.js
+++ scrapper-main/main.js
@@ -11,9 +11,28 @@ axios(URL)
 		const countriesWithRates = $(
 			'.table.table-bordered.table-hover.downloads > tbody > tr'
 		);
+
+		let dataObj = new Object(); 
 		countriesWithRates.each(function () {
 			const rates = $(this).find('td').text();
-			console.log(rates);
+			//console.log(rates);
+			let newStr = rates.split('\t');
+			newStr.shift();
+			console.log(newStr);
+			//formatStr(newStr, dataObj)
 		});
 	})
 	.catch((err) => console.log(err));
+
+
+
+	function formatStr(arr, dataObj){
+		// regex to match all the words before the first digit
+		let regExp = /[^A-Z]*(^\D+)/ 
+		let newArr = arr[0].split(regExp); // split array element 0 using the regExp rule
+		dataObj[newArr[1]] = newArr[2]; // store object 
+		console.log(dataObj)
+	}
+
+
+
